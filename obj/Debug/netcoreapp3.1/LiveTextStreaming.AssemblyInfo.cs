cdea6e428512f42e1f089de7db137b3adfaa8d23
Maciej Młotkowski <mmacie@wp.pl>
2020-02-27 00:23:43 +0100

    azure signalr service integration

--- obj/Debug/netcoreapp3.1/LiveTextStreaming.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/LiveTextStreaming.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2cf762d6-5641-4d32-b483-5d7cf2c8f5e1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveTextStreaming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
